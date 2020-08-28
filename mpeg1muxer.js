f23b144a8c2482a996a7e492bae3dcca845e93fc
Seyd <34152761+SeydX@users.noreply.github.com>
2020-08-28 04:47:21 +0200

    Changes for homebridge-camera-ui

--- mpeg1muxer.js
+++ mpeg1muxer.js
@@ -1,29 +1,26 @@
-var Mpeg1Muxer, child_process, events, util
+const child_process = require('child_process');
+const util = require('util');
+const events = require('events');   
 
-child_process = require('child_process')
-
-util = require('util')
-
-events = require('events')
-
-Mpeg1Muxer = function(options) {
-  var key
-  this.url = options.url
-  this.ffmpegOptions = options.ffmpegOptions
-  this.exitCode = undefined
-  this.additionalFlags = []
+const Mpeg1Muxer = function(options, log, debug) {
+  var key;
+  this.log = log;
+  this.debug = debug;
+  this.name = options.name;
+  this.url = options.url;
+  this.ffmpegOptions = options.ffmpegOptions;
+  this.exitCode = undefined;
+  this.additionalFlags = [];
   if (this.ffmpegOptions) {
     for (key in this.ffmpegOptions) {
-      this.additionalFlags.push(key)
+      this.additionalFlags.push(key);
       if (String(this.ffmpegOptions[key]) !== '') {
-        this.additionalFlags.push(String(this.ffmpegOptions[key]))
+        this.additionalFlags.push(String(this.ffmpegOptions[key]));
       }
     }
-  }
+  }     
   this.spawnOptions = [
-    "-rtsp_transport",
-    "tcp",
-    "-i",
+    '-i',
     this.url,
     '-f',
     'mpegts',
@@ -32,27 +29,30 @@ Mpeg1Muxer = function(options) {
     // additional ffmpeg options go here
     ...this.additionalFlags,
     '-'
-  ]
+  ];   
   this.stream = child_process.spawn(options.ffmpegPath, this.spawnOptions, {
     detached: false
-  })
-  this.inputStreamStarted = true
+  });
+  this.inputStreamStarted = true;
   this.stream.stdout.on('data', (data) => {
-    return this.emit('mpeg1data', data)
-  })
+    return this.emit('mpeg1data', data);
+  });
   this.stream.stderr.on('data', (data) => {
-    return this.emit('ffmpegStderr', data)
-  })
+    return this.emit('ffmpegStderr', data);
+  });
   this.stream.on('exit', (code, signal) => {
+    this.inputStreamStarted = false;
     if (code === 1) {
-      console.error('RTSP stream exited with error')
-      this.exitCode = 1
-      return this.emit('exitWithError')
+      this.debug('%s: RTSP stream exited with error', this.name);
+      this.exitCode = 1;
+      return this.emit('exitWithError');
+    } else {
+      this.debug('%s: Stream Exit (' + (code ? code : signal) + ')', this.name);
     }
-  })
-  return this
-}
+  }); 
+  return this;
+};    
 
-util.inherits(Mpeg1Muxer, events.EventEmitter)
+util.inherits(Mpeg1Muxer, events.EventEmitter);
 
-module.exports = Mpeg1Muxer
+module.exports = Mpeg1Muxer;
--- videoStream.js
+++ videoStream.js
@@ -1,134 +1,176 @@
-var Mpeg1Muxer, STREAM_MAGIC_BYTES, VideoStream, events, util, ws
+const ws = require('ws');
+const util = require('util');
+const events = require('events');
 
-ws = require('ws')
+const Mpeg1Muxer = require('./mpeg1muxer');  
 
-util = require('util')
+const STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes   
 
-events = require('events')
+const VideoStream = function(options, log, debug, api) {
+  this.options = options;
+  this.name = options.name;
+  this.streamUrl = options.streamUrl;
+  this.width = options.width;
+  this.height = options.height;
+  this.wsPort = options.wsPort;
+  this.inputStreamStarted = false;
+  this.stream = undefined;
+  this.log = log;
+  this.debug = debug;
+  
+  this.quit = false;
+  
+  api.on('shutdown', () => {
+    this.stopAll();
+  });
+  
+  return this;
+};
 
-Mpeg1Muxer = require('./mpeg1muxer')
+util.inherits(VideoStream, events.EventEmitter);
 
-STREAM_MAGIC_BYTES = "jsmp" // Must be 4 bytes
+VideoStream.prototype.stopAll = function() {
+  
+  this.log('%s: Closing streaming server..', this.name);
+  this.quit = true;
+  
+  this.wsServer.close();
+  this.stream.kill();
+  this.inputStreamStarted = false;
+  return this;
+};
 
-VideoStream = function(options) {
-  this.options = options
-  this.name = options.name
-  this.streamUrl = options.streamUrl
-  this.width = options.width
-  this.height = options.height
-  this.wsPort = options.wsPort
-  this.inputStreamStarted = false
-  this.stream = undefined
-  this.startMpeg1Stream()
-  this.pipeStreamToSocketServer()
-  return this
-}
-
-util.inherits(VideoStream, events.EventEmitter)
-
-VideoStream.prototype.stop = function() {
-  this.wsServer.close()
-  this.stream.kill()
-  this.inputStreamStarted = false
-  return this
-}
+VideoStream.prototype.stopStream = function() {
+  this.stream.kill();
+  this.inputStreamStarted = false;
+  return this;
+};
 
 VideoStream.prototype.startMpeg1Stream = function() {
-  var gettingInputData, gettingOutputData, inputData, outputData
+  var gettingInputData, gettingOutputData, inputData, outputData;
   this.mpeg1Muxer = new Mpeg1Muxer({
+    name: this.name,
     ffmpegOptions: this.options.ffmpegOptions,
     url: this.streamUrl,
-    ffmpegPath: this.options.ffmpegPath == undefined ? "ffmpeg" : this.options.ffmpegPath
-  })
-  this.stream = this.mpeg1Muxer.stream
-  if (this.inputStreamStarted) {
-    return
+    ffmpegPath: this.options.ffmpegPath == undefined ? 'ffmpeg' : this.options.ffmpegPath
+  }, this.log, this.debug);
+  this.stream = this.mpeg1Muxer.stream;
+  if (!this.mpeg1Muxer.inputStreamStarted) {
+    return;
+  } else {
+    this.debug('%s: Streaming started - Stream to ' + this.options.streamUrl + ' to http://localhost:' + this.wsPort + '/', this.name);
+    this.inputStreamStarted = true;
   }
   this.mpeg1Muxer.on('mpeg1data', (data) => {
-    return this.emit('camdata', data)
-  })
-  gettingInputData = false
-  inputData = []
-  gettingOutputData = false
-  outputData = []
+    return this.emit('camdata', data);
+  });
+  gettingInputData = false;
+  inputData = [];
+  gettingOutputData = false;
+  outputData = [];
   this.mpeg1Muxer.on('ffmpegStderr', (data) => {
-    var size
-    data = data.toString()
+    var size;
+    data = data.toString();
     if (data.indexOf('Input #') !== -1) {
-      gettingInputData = true
+      gettingInputData = true;
     }
     if (data.indexOf('Output #') !== -1) {
-      gettingInputData = false
-      gettingOutputData = true
+      gettingInputData = false;
+      gettingOutputData = true;
     }
     if (data.indexOf('frame') === 0) {
-      gettingOutputData = false
+      gettingOutputData = false;
     }
     if (gettingInputData) {
-      inputData.push(data.toString())
-      size = data.match(/\d+x\d+/)
+      inputData.push(data.toString());
+      size = data.match(/\d+x\d+/);
       if (size != null) {
-        size = size[0].split('x')
+        size = size[0].split('x');
         if (this.width == null) {
-          this.width = parseInt(size[0], 10)
+          this.width = parseInt(size[0], 10);
         }
         if (this.height == null) {
-          return this.height = parseInt(size[1], 10)
+          return this.height = parseInt(size[1], 10);
         }
       }
     }
-  })
+  });
   this.mpeg1Muxer.on('ffmpegStderr', function(data) {
-    return global.process.stderr.write(data)
-  })
+    return global.process.stderr.write(data);
+  });
   this.mpeg1Muxer.on('exitWithError', () => {
-    return this.emit('exitWithError')
-  })
-  return this
-}
+    return this.emit('exitWithError');
+  });
+  return this;
+};
 
 VideoStream.prototype.pipeStreamToSocketServer = function() {
   this.wsServer = new ws.Server({
     port: this.wsPort
-  })
-  this.wsServer.on("connection", (socket, request) => {
-    return this.onSocketConnect(socket, request)
-  })
+  });
+  
+  this.debug('%s Awaiting WebSocket connections on ws://localhost:' + this.wsPort + '/', this.name);
+  
+  this.wsServer.on('connection', (socket, request) => {
+    return this.onSocketConnect(socket, request);
+  });
   this.wsServer.broadcast = function(data, opts) {
-    var results
-    results = []
+    var results;
+    results = [];
     for (let client of this.clients) {
       if (client.readyState === 1) {
-        results.push(client.send(data, opts))
+        results.push(client.send(data, opts));
       } else {
-        results.push(console.log("Error: Client from remoteAddress " + client.remoteAddress + " not connected."))
+        results.push(this.log('%s: Error: Client from remoteAddress ' + client.remoteAddress + ' not connected.', this.name));
       }
     }
-    return results
-  }
+    return results;
+  };
   return this.on('camdata', (data) => {
-    return this.wsServer.broadcast(data)
-  })
-}
+    return this.wsServer.broadcast(data);
+  });
+};
 
 VideoStream.prototype.onSocketConnect = function(socket, request) {
-  var streamHeader
+  var streamHeader;
   // Send magic bytes and video size to the newly connected socket
   // struct { char magic[4]; unsigned short width, height;}
-  streamHeader = new Buffer(8)
-  streamHeader.write(STREAM_MAGIC_BYTES)
-  streamHeader.writeUInt16BE(this.width, 4)
-  streamHeader.writeUInt16BE(this.height, 6)
+  streamHeader = new Buffer(8);
+  streamHeader.write(STREAM_MAGIC_BYTES);
+  streamHeader.writeUInt16BE(this.width, 4);
+  streamHeader.writeUInt16BE(this.height, 6);
   socket.send(streamHeader, {
     binary: true
-  })
-  console.log(`${this.name}: New WebSocket Connection (` + this.wsServer.clients.size + " total)")
+  });
+  this.debug(`${this.name}: New WebSocket Connection (` + this.wsServer.clients.size + ' total)');
+  
+  if(this.reload)
+    clearTimeout(this.reload);
 
-  socket.remoteAddress = request.connection.remoteAddress
+  socket.remoteAddress = request.connection.remoteAddress;
 
-  return socket.on("close", (code, message) => {
-    return console.log(`${this.name}: Disconnected WebSocket (` + this.wsServer.clients.size + " total)")
-  })
-}
+  return socket.on('close', (code, message) => {   
+    
+    if(!this.wsServer.clients.size && !this.quit)
+      this.debug('%s: If no clients connects to the Websocket, the stream will be closed in 10s.', this.name);
+    
+    this.reload = setTimeout(() => {  //check if user just reload page
+       
+      if(!this.wsServer.clients.size){
+        this.stopStream();
+        this.inputStreamStarted = false;
+      }
+      
+      this.reload = false;            
+    
+    }, 10000); //wait 10s till close       
+    
+    return this.debug(`${this.name}: Disconnected WebSocket (` + this.wsServer.clients.size + ' total)');
+  });
+};
 
-module.exports = VideoStream
+VideoStream.prototype.streamRunning = function() {
+  return this.inputStreamStarted;
+};
+
+module.exports = VideoStream;
