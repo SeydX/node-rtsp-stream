4a9dcd040f887eb068d808300a5bcd1af6482438
Seyd <34152761+SeydX@users.noreply.github.com>
2021-03-20 21:51:23 +0100

    refactored

--- mpeg1muxer.js
+++ mpeg1muxer.js
@@ -1,29 +1,27 @@
+'use strict';
+
 const child_process = require('child_process');
-const util = require('util');
 const events = require('events');   
+const util = require('util');
 
 const Mpeg1Muxer = function(options, Logger, streamSessions) {
-  var key;
-  this.name = options.name;
-  this.url = options.url;
-  this.width = options.width;
-  this.height = options.height;
-  this.ffmpegOptions = options.ffmpegOptions;
-  this.wsPort = options.wsPort;
-  this.exitCode = undefined;
-  this.additionalFlags = [];
+  
   this.Logger = Logger;
+  this.options = options;
+  this.cameraName = options.name;
   this.streamSessions = streamSessions;
-  if (this.ffmpegOptions) {
-    for (key in this.ffmpegOptions) {
-      this.additionalFlags.push(key);
-      if (String(this.ffmpegOptions[key]) !== '') {
-        this.additionalFlags.push(String(this.ffmpegOptions[key]));
-      }
+  
+  //this.exitCode; 
+  this.additionalFlags = [];
+  
+  if(this.options.ffmpegOptions) {
+    for(const key of Object.keys(this.options.ffmpegOptions)){
+      this.additionalFlags.push(key, this.options.ffmpegOptions[key]);
     }
   }     
+  
   this.spawnOptions = [
-    this.url,
+    this.options.url,
     '-f',
     'mpegts',
     '-codec:v',
@@ -32,30 +30,39 @@ const Mpeg1Muxer = function(options, Logger, streamSessions) {
     '-'
   ].flat();
   
-  this.Logger.ui.debug('Stream command: ' + options.ffmpegPath + ' ' + this.spawnOptions.toString().replace(/,/g, ' '), this.name);
-  this.stream = child_process.spawn(options.ffmpegPath, this.spawnOptions, {
+  this.Logger.ui.debug('Stream command: ' + this.options.ffmpegPath + ' ' + this.spawnOptions.toString().replace(/,/g, ' '), this.cameraName);
+  
+  this.stream = child_process.spawn(this.options.ffmpegPath, this.spawnOptions, {
     detached: false
   });
-  this.Logger.ui.debug('Streaming started - Stream from ' + this.url, this.name);
-  this.inputStreamStarted = true;
+  
+  this.Logger.ui.debug('Streaming started - Stream from ' + this.options.url, this.cameraName);
+  
   this.stream.stdout.on('data', (data) => {
     return this.emit('mpeg1data', data);
   });
+  
   this.stream.stderr.on('data', (data) => {
     return this.emit('ffmpegStderr', data);
   });
+  
   this.stream.on('exit', (code, signal) => {
-    this.inputStreamStarted = false;
-    this.streamSessions.closeSession(this.name);
+  
     if (code === 1) {
-      this.Logger.ui.error('RTSP stream exited with error! (' + signal + ')', this.name);
-      this.exitCode = 1;
-      return this.emit('exitWithError');
+      this.Logger.ui.error('RTSP stream exited with error! (' + signal + ')', this.cameraName);
     } else {
-      this.Logger.ui.debug('Stream Exit (' + (code ? code : signal) + ')', this.name);
+      this.Logger.ui.debug('Stream Exit (expected)', this.cameraName);
     }
+  
+    return this.emit('streamExit', {
+      code: code,
+      signal: signal
+    });
+    
   }); 
+  
   return this;
+
 };    
 
 util.inherits(Mpeg1Muxer, events.EventEmitter);
