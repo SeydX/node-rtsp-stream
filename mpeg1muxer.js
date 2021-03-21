2b32b4ff973aea940825651cdfac1e35d5749de4
Seyd <34152761+SeydX@users.noreply.github.com>
2021-03-21 12:39:29 +0100

    Update mpeg1muxer.js

--- mpeg1muxer.js
+++ mpeg1muxer.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const child_process = require('child_process');
-const events = require('events');   
+const events = require('events'); 
+const readline = require('readline');  
 const util = require('util');
 
 const Mpeg1Muxer = function(options, Logger, streamSessions) {
@@ -21,14 +22,14 @@ const Mpeg1Muxer = function(options, Logger, streamSessions) {
   }     
   
   this.spawnOptions = [
-    this.options.url,
+    ...this.options.url,
     '-f',
     'mpegts',
     '-codec:v',
     'mpeg1video',
     ...this.additionalFlags,
     '-'
-  ].flat();
+  ];
   
   this.Logger.ui.debug('Stream command: ' + this.options.ffmpegPath + ' ' + this.spawnOptions.toString().replace(/,/g, ' '), this.cameraName);
   
@@ -36,7 +37,7 @@ const Mpeg1Muxer = function(options, Logger, streamSessions) {
     detached: false
   });
   
-  this.Logger.ui.debug('Streaming started - Stream from ' + this.options.url, this.cameraName);
+  this.Logger.ui.debug('Streaming started - Stream from ' + this.options.url[this.options.url.length-1], this.cameraName);
   
   this.stream.stdout.on('data', (data) => {
     return this.emit('mpeg1data', data);
@@ -44,6 +45,19 @@ const Mpeg1Muxer = function(options, Logger, streamSessions) {
   
   this.stream.stderr.on('data', (data) => {
     return this.emit('ffmpegStderr', data);
+  });
+  
+  const stderr = readline.createInterface({
+    input: this.stream.stderr,
+    terminal: false
+  });
+  
+  stderr.on('line', line => {
+    if (line.match(/\[(panic|fatal|error)\]/)) {
+      this.Logger.ui.error(line, this.cameraName);
+    } else {
+      this.Logger.ui.debug(line, this.cameraName);
+    }
   });
   
   this.stream.on('exit', (code, signal) => {
