138d680bfcce502ec7e566b88a9aafed000f7b31
Seyd <34152761+SeydX@users.noreply.github.com>
2021-04-22 07:02:31 +0200

    Update videoStream.js

--- videoStream.js
+++ videoStream.js
@@ -1,303 +1,84 @@
 'use strict';
 
-const events = require('events');
-const https = require('https');
+const child_process = require('child_process');
+const events = require('events'); 
+const readline = require('readline');  
 const util = require('util');
-const ws = require('ws');
 
-const Mpeg1Muxer = require('./mpeg1muxer');  
-
-const STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes   
-
-const VideoStream = function(options, logger, streamSessions) {
+const Mpeg1Muxer = function(options, logger, streamSessions) {
   
   this.logger = logger;
-  
   this.options = options;
   this.cameraName = options.name;
-  this.reloadTimer = options.reloadTimer * 1000 || 30000;
-
-  this.streamSessions = streamSessions; 
+  this.streamSessions = streamSessions;
+  
+  //this.exitCode; 
+  this.additionalFlags = [];
+  
+  if(this.options.ffmpegOptions) {
+    for(const key of Object.keys(this.options.ffmpegOptions)){
+      this.additionalFlags.push(key, this.options.ffmpegOptions[key]);
+    }
+  }     
+  
+  this.spawnOptions = [
+    ...this.options.url,
+    '-f',
+    'mpegts',
+    '-codec:v',
+    'mpeg1video',
+    ...this.additionalFlags,
+    '-'
+  ];
+  
+  this.logger.debug(`Stream command: ${this.options.ffmpegPath} ${this.spawnOptions.toString().replace(/,/g, ' ')}`, this.cameraName, '[Streams]');
+  
+  this.stream = child_process.spawn(this.options.ffmpegPath, this.spawnOptions, {
+    detached: false
+  });
+  
+  this.logger.debug(`Streaming started - Stream from ${this.options.url[this.options.url.length-1]}`, this.cameraName, '[Streams]');
+  
+  this.stream.stdout.on('data', (data) => {
+    return this.emit('mpeg1data', data);
+  });
+  
+  this.stream.stderr.on('data', (data) => {
+    return this.emit('ffmpegStderr', data);
+  });
+  
+  const stderr = readline.createInterface({
+    input: this.stream.stderr,
+    terminal: false
+  });
+  
+  stderr.on('line', line => {
+    if (line.match(/\[(panic|fatal|error)\]/)) {
+      this.logger.error(line, this.cameraName, '[Streams]');
+    } else {
+      this.logger.debug(line, this.cameraName, '[Streams]');
+    }
+  });
+  
+  this.stream.on('exit', (code, signal) => {
+  
+    if (code === 1) {
+      this.logger.error(`RTSP stream exited with error! (${signal})`, this.cameraName, '[Streams]');
+    } else {
+      this.logger.debug(`Stream Exit (expected)`, this.cameraName, '[Streams]');
+    }
+  
+    return this.emit('streamExit', {
+      code: code,
+      signal: signal
+    });
+    
+  }); 
   
   return this;
 
-};
+};    
 
-util.inherits(VideoStream, events.EventEmitter);
+util.inherits(Mpeg1Muxer, events.EventEmitter);
 
-VideoStream.prototype = {
-
-  ping: function(){},  
-  
-  heartbeat: function(socket, msg){ 
-  
-    if(msg && msg === '--heartbeat--'){ //from client    
-      //this.logger.debug('received heartbeat from client', this.cameraName + ' (' + socket.remoteAddress + ')');
-      socket.send('--ping--');
-    } else {
-      this.logger.debug(`WebSocket: received heartbeat from socket ${socket.remoteAddress}`, this.cameraName, '[Streams]');
-    }  
-    
-    socket.isAlive = true;            
-  
-  },
-  
-  pipeStreamToServer: function(){
-                                                         
-    if(this.options.ssl){ 
-      
-      const server = https.createServer({
-        cert: this.options.ssl.cert,
-        key: this.options.ssl.key
-      }).listen(this.options.wsPort);
-          
-      this.WebSocket = new ws.Server({ server: server, perMessageDeflate:false });
-    
-    } else {    
-    
-      this.WebSocket = new ws.Server({
-        port: this.options.wsPort,
-        perMessageDeflate:false 
-      });
-
-    }
-
-    this.WebSocket.on('error', (error) => {
-      let err;
-   
-      if (error.syscall !== 'listen'){
-        logger.error(error, false, true);
-      }
-
-      let bind = typeof port === 'string'
-        ? 'Pipe ' + this.options.wsPort
-        : 'Socket port ' + this.options.wsPort;
-
-      switch (error.code) {
-        case 'EACCES':
-          err = `Can not connect to stream server! ${bind} requires elevated privileges`;
-          break;
-        case 'EADDRINUSE':
-          err = `Can not connect to stream server! ${bind} is already in use`;
-          break;
-        default:
-          err = error;
-      }
-
-      this.logger.error(err, this.cameraName, '[Streams]');
-    });
-    
-    this.WebSocket.on('listening', () => {
-      this.logger.debug(`Awaiting WebSocket connections on ${this.options.ssl ? 'wss' : 'ws'}://localhost:${this.options.wsPort}`, this.cameraName, '[Streams]');
-    });
-    
-    this.WebSocket.on('connection', (socket, request) => {
-      return this.onConnection(socket, request);
-    });
-  
-    this.pingInterval = setInterval(() => {
-    
-      if(this.WebSocket.clients.size)
-        this.logger.debug(`Pinging sockets..`, this.cameraName, '[Streams]');
-    
-      this.WebSocket.clients.forEach(socket => {
-        
-        if (socket.isAlive === false)
-          return socket.terminate();
-        
-        socket.isAlive = false;
-        socket.ping(this.ping.bind(this));
-      
-      });
-    
-    }, 10000);
-    
-    return;
-  
-  },
-  
-  onConnection: function(socket, request) {
-  
-    let streamHeader, allowStream;
-    
-    // Send magic bytes and video size to the newly connected socket
-    // struct { char magic[4]; unsigned short width, height;}
-    streamHeader = new Buffer(8);
-    streamHeader.write(STREAM_MAGIC_BYTES);
-    streamHeader.writeUInt16BE(this.options.width, 4);
-    streamHeader.writeUInt16BE(this.options.height, 6);
-    
-    socket.send(streamHeader, {
-      binary: true
-    });
-    
-    socket.isAlive = true; 
-    socket.remoteAddress = request.connection.remoteAddress;
-    socket.on('pong', this.heartbeat.bind(this, socket));
-    socket.on('message', this.heartbeat.bind(this, socket));
-    
-    this.logger.debug(`New WebSocket connection from ${socket.remoteAddress} (${this.WebSocket.clients.size} total)`, this.cameraName, '[Streams]');
-      
-    if(this.streamTimeout){
-      clearTimeout(this.streamTimeout);
-      this.streamTimeout = null;
-    }
-  
-    if(this.WebSocket.clients.size && !this.mpeg1Muxer){
-      
-      allowStream = this.streamSessions.requestSession(this.cameraName);
-      
-      if(allowStream)
-        this.startStream();
-    
-    }
-    
-    socket.on('close', () => {
-      
-      this.logger.debug(`${socket.remoteAddress} disconnected or closed from WebSocket (${this.WebSocket.clients.size} total)`, this.cameraName, '[Streams]');
-      
-      if(!this.WebSocket.clients.size){
-      
-        this.logger.debug(`If no clients connects to the Websocket, the stream will be closed in ${this.reloadTimer/1000}s`, this.cameraName, '[Streams]');
-        
-        this.streamTimeout = setTimeout(() => {  //check if user just reload page
-           
-          if(!this.WebSocket.clients.size)
-            this.stopStream();
-          
-          this.streamTimeout = null;            
-        
-        }, this.reloadTimer);
-      
-      } 
-    
-    });
-    
-    return;
-  
-  },
-  
-  onBroadcast: function(data, opts) {
-  
-    const results = [];
-
-    for (const client of this.WebSocket.clients) {
-      if (client.readyState === 1) {
-        results.push(client.send(data, opts));
-      } else {
-        results.push((`${this.cameraName}: Error: Client from remoteAddress ${client.remoteAddress} not connected.`));
-      }
-    }
-    
-    return results;
-  
-  },
-  
-  startStream: function(){
-  
-    let gettingInputData = false;
-    //let gettingOutputData = false;
-    
-    const inputData = [];
-    //const outputData = [];
-    
-    const mpegOptions = {
-      name: this.cameraName,
-      width: this.options.width,
-      height: this.options.height,
-      ffmpegOptions: this.options.ffmpegOptions,
-      url: this.options.streamUrl,
-      ffmpegPath: !this.options.ffmpegPath
-        ? 'ffmpeg' 
-        : this.options.ffmpegPath,
-      ssl: this.options.ssl
-    };
-    
-    this.mpeg1Muxer = new Mpeg1Muxer(mpegOptions, this.logger, this.streamSessions);
-    
-    this.mpeg1Muxer.on('mpeg1data', (data) => {
-      return this.onBroadcast(data);
-    });
-
-    this.mpeg1Muxer.on('ffmpegStderr', (data) => {
-      
-      let size;
-      data = data.toString();
-      
-      if(data.indexOf('Input #') !== -1){
-        gettingInputData = true;
-      }
-      
-      if(data.indexOf('Output #') !== -1){
-        gettingInputData = false;
-        //gettingOutputData = true;
-      }
-      
-      /*if(data.indexOf('frame') === 0){
-        gettingOutputData = false;
-      }*/
-      
-      if(gettingInputData){
-        
-        inputData.push(data);
-        
-        size = data.match(/\d+x\d+/);
-        
-        if(size != null){
-          
-          size = size[0].split('x');
-          
-          if(this.options.width == null){
-            this.options.width = parseInt(size[0], 10);
-          }
-          
-          if(this.options.height == null){
-            this.options.height = parseInt(size[1], 10);
-          }
-        
-        }
-      
-      }
-      
-      return;
-    
-    });
-    
-    this.mpeg1Muxer.on('streamExit', () => {
-      this.mpeg1Muxer = false;
-      this.streamSessions.closeSession(this.cameraName);
-      return;
-    });
-    
-    return;
-  
-  },
-  
-  stopStream: function(){
-  
-    if(this.mpeg1Muxer && this.mpeg1Muxer.stream){
-      this.logger.debug('Closing Stream..', this.cameraName, '[Streams]');
-      this.mpeg1Muxer.stream.kill();
-    }
-      
-    return;
-
-  },
-  
-  destroy: function(){
-
-    this.logger.debug(`Closing WebSocket and Stream..`, this.cameraName, '[Streams]');
-    
-    if(this.WebSocket)
-      this.WebSocket.close();
-    
-    if(this.mpeg1Muxer && this.mpeg1Muxer.stream)
-      this.mpeg1Muxer.stream.kill();
-  
-    return;
-  
-  }
-  
-};
-
-module.exports = VideoStream;
+module.exports = Mpeg1Muxer;
